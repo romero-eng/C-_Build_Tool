28c5a5ca4e67ef1b6fc51dd6f21daf7abdee9f5f
romero-eng <39012010+romero-eng@users.noreply.github.com>
2024-08-16 18:01:43 -0400

    Update build.py
    Separated compilation into object generation and linking

--- build.py
+++ build.py
@@ -67,17 +67,19 @@ def get_compiler_warning_flags(turn_on_warnings: str | list[str] = []) -> str:
     return ' '.join([f'-W{flag:s}' for warning, flag in WARNING_DECISIONS.items() if warning in turn_on_warnings])
 
 
-def run_command(command: str,
-                command_description: str,
-                successful_return_code: int = 0):
+def run_command(command_description: str,
+                command: str,
+                successful_return_code: int = 0) -> bool:
 
     results: subprocess.CompletedProcess = \
         subprocess.run(command,
                        stdout=subprocess.PIPE,
                        stderr=subprocess.PIPE)
 
+    success: bool = results.returncode == successful_return_code
+
     formatted_results: list[str] = [f'\tCommand: {command:s}']
-    formatted_results.append(f'\tCommand Results: {'Succesful' if results.returncode == successful_return_code else 'Failure':s}')
+    formatted_results.append(f'\tCommand Results: {'Succesful' if success else 'Failure':s}')
 
     if results.stdout:
         formatted_results.append(f'\tOutput:\n\n{results.stdout.decode('utf-8'):s}')
@@ -87,23 +89,41 @@ def run_command(command: str,
 
     print(f'\n{command_description:s}:\n{'':{'-':s}>{len(command_description) + 1:d}s}\n{'\n\n'.join(formatted_results):s}\n')
 
+    return success
+
 
 if (__name__=='__main__'):
 
-    cpp_files = ['HelloWorld', 'Add']
-    executable_name = 'HelloWorld'
+    executable_name = 'present_addition'
+    cpp_files = ['main', 'Add']
 
-    c_plus_plus_compile_command: str = 'g++ {source_files} -o {executable}.exe {build_configuration:s} {language_standard:s} {warnings:s} {miscellaneous:s}'
+    compile_command: str = 'g++ -c {source_file}.cpp {build_configuration:s} {language_standard:s} {warnings:s} {miscellaneous:s}'
+    link_command: str = 'g++ -o {executable}.exe {object_files:s}'
 
-    run_command(c_plus_plus_compile_command.format(source_files=' '.join([f'{cpp_file:s}.cpp' for cpp_file in cpp_files]),
-                                                          executable=executable_name,
-                                                          build_configuration=get_build_configuration_flags('Debug'),
-                                                               language_standard=get_language_standard_flag('C++ 2020'),
-                                                                       warnings=get_compiler_warning_flags(['Treat warnings as errors',
-                                                                                                             'Avoid a lot of questionable coding practices',
-                                                                                                             'Avoid even more questionable coding practices',
-                                                                                                             'Follow Effective C++ Style Guidelines',
-                                                                                                             'Avoid potentially value-changing implicit conversions',
-                                                                                                             'Avoid potentially sign-changing implicit conversions for integers']),
-                                                                      miscellaneous=get_miscellaneous_flags('Disable Compiler Extensions')),
-                'Compilation Results')
+    build_configuration_flags: str = get_build_configuration_flags('Debug')
+    language_standard_flags: str = get_language_standard_flag('C++ 2020')
+    miscellaneous_flags:str = get_miscellaneous_flags('Disable Compiler Extensions')
+    warning_flags: str = \
+        get_compiler_warning_flags(['Treat warnings as errors',
+                                    'Avoid a lot of questionable coding practices',
+                                    'Avoid even more questionable coding practices',
+                                    'Follow Effective C++ Style Guidelines',
+                                    'Avoid potentially value-changing implicit conversions',
+                                    'Avoid potentially sign-changing implicit conversions for integers'])
+
+    for file in cpp_files:
+        success = \
+            run_command(f'{file:s}.cpp Compilation Results',
+                        compile_command.format(source_file=file,
+                                               executable=executable_name,
+                                               build_configuration=build_configuration_flags,
+                                               language_standard=language_standard_flags,
+                                               warnings=warning_flags,
+                                               miscellaneous=miscellaneous_flags))
+        if not success:
+            break
+
+    if success:
+        run_command('Linking Results',
+                    link_command.format(executable=executable_name,
+                                        object_files=' '.join([f'{file:s}.o' for file in cpp_files])))
