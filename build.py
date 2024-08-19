a47ce34157986d8e45759870f58f8d19155eec64
romero-eng <39012010+romero-eng@users.noreply.github.com>
2024-08-18 23:57:22 -0400

    Update build.py
    Moved generation and linking of object files into separate respective functions with the added ability to specific source and build directories

--- build.py
+++ build.py
@@ -1,4 +1,6 @@
+import os
 import subprocess
+from typing import Optional
 
 import flags
 
@@ -28,38 +30,76 @@ def run_command(command_description: str,
     return success
 
 
+def generate_object_files(build_configuration: str,
+                          language_standard: str,
+                          miscellaneous: str,
+                          warnings: list[str],
+                          source_dir: Optional[str] = None,
+                          build_dir: Optional[str] = None) -> bool:
+
+    compile_command: str = 'g++ -c {{source_file_path:s}}.cpp -o {{object_file_path:s}}.o {build_configuration:s} {language_standard:s} {warnings:s} {miscellaneous:s}'
+
+    compile_command_with_flags: str = \
+        compile_command.format(build_configuration=flags.get_build_configuration_flags(build_configuration),
+                               language_standard=flags.get_language_standard_flag(language_standard),
+                               warnings=flags.get_compiler_warning_flags(warnings),
+                               miscellaneous=flags.get_miscellaneous_flags(miscellaneous))
+
+    success: bool = True
+
+    for file in cpp_files:
+
+        success = \
+            run_command(f'{file:s}.cpp Compilation Results',
+                        compile_command_with_flags.format(source_file_path=os.path.join(source_dir, file) if source_dir else file,
+                                                          object_file_path=os.path.join( build_dir, file) if  build_dir else file))
+
+        if not success:
+            break
+
+    return success
+
+
+def link_object_files_into_executable(executable_name: str,
+                                      object_file_names: list[str],
+                                      build_dir: Optional[str] = None):
+
+    link_command: str = 'g++ -o {executable}.exe {object_files:s}'
+
+    if success:
+        run_command('Linking Results',
+                    link_command.format(executable=os.path.join(build_dir, f'{executable_name:s}.o') if build_dir else executable_name,
+                                        object_files=' '.join([os.path.join(build_dir, f'{file_name:s}.o') if build_dir else f'{file_name:s}.o' for file_name in object_file_names])))
+
+
 if (__name__=='__main__'):
 
     executable_name = 'present_addition'
     cpp_files = ['main', 'Add']
 
-    compile_command: str = 'g++ -c {source_file}.cpp {build_configuration:s} {language_standard:s} {warnings:s} {miscellaneous:s}'
-    link_command: str = 'g++ -o {executable}.exe {object_files:s}'
+    build_configuration: str = 'Debug'
+    language_standard: str = 'C++ 2020'
+    miscellaneous: str = 'Disable Compiler Extensions'
+    warnings: list[str] = \
+        ['Treat warnings as errors',
+          'Avoid a lot of questionable coding practices',
+          'Avoid even more questionable coding practices',
+          'Follow Effective C++ Style Guidelines',
+          'Avoid potentially value-changing implicit conversions',
+          'Avoid potentially sign-changing implicit conversions for integers']
 
-    build_configuration_flags: str = flags.get_build_configuration_flags('Debug')
-    language_standard_flags: str = flags.get_language_standard_flag('C++ 2020')
-    miscellaneous_flags:str = flags.get_miscellaneous_flags('Disable Compiler Extensions')
-    warning_flags: str = \
-        flags.get_compiler_warning_flags(['Treat warnings as errors',
-                                          'Avoid a lot of questionable coding practices',
-                                          'Avoid even more questionable coding practices',
-                                          'Follow Effective C++ Style Guidelines',
-                                          'Avoid potentially value-changing implicit conversions',
-                                          'Avoid potentially sign-changing implicit conversions for integers'])
+    if not os.path.exists('build'):
+        os.mkdir('build')
 
-    for file in cpp_files:
-        success = \
-            run_command(f'{file:s}.cpp Compilation Results',
-                        compile_command.format(source_file=file,
-                                               executable=executable_name,
-                                               build_configuration=build_configuration_flags,
-                                               language_standard=language_standard_flags,
-                                               warnings=warning_flags,
-                                               miscellaneous=miscellaneous_flags))
-        if not success:
-            break
+    success = \
+        generate_object_files(build_configuration,
+                              language_standard,
+                              miscellaneous,
+                              warnings,
+                              'src',
+                              'build')
 
     if success:
-        run_command('Linking Results',
-                    link_command.format(executable=executable_name,
-                                        object_files=' '.join([f'{file:s}.o' for file in cpp_files])))
+        link_object_files_into_executable(executable_name,
+                                          cpp_files,
+                                          'build')
