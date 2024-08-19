4b49e2683fdeac4b2c5039bd8cce33f53e582d16
romero-eng <39012010+romero-eng@users.noreply.github.com>
2024-08-19 16:20:16 -0400

    Update build.py
    implemented retrieval of C++ source files over traversal of source directory

--- src/build.py
+++ src/build.py
@@ -3,19 +3,33 @@ import os
 import compile
 
 
-if (__name__ == '__main__'):
+def get_file_paths(source_directory: str) -> tuple[list[str], list[str], str]:
 
-    executable_name: str = 'present_addition'
-    source_directory: str = os.path.join(os.getcwd(), 'sample_C++_code', 'src')
-    build_directory: str = os.path.join(os.getcwd(), 'sample_C++_code', 'build')
-    cpp_file_names: list[str] = ['main', 'Add']
+    build_directory: str = os.path.join(os.path.dirname(source_directory), 'build')
 
     if not os.path.exists(build_directory):
         os.mkdir(build_directory)
 
-    compile.build_executable_from_source([os.path.join(source_directory, f'{file_name:s}.cpp') for file_name in cpp_file_names],
-                                         [os.path.join(build_directory, f'{file_name:s}.o') for file_name in cpp_file_names],
-                                         os.path.join(build_directory, f'{executable_name:s}.exe'),
+    cpp_file_paths: list[str] = [os.path.join(root, file) for root, _, files in os.walk(source_directory) for file in files if os.path.splitext(file)[1] == '.cpp']  # noqa: E501
+    object_file_paths: list[str] = [f'{os.path.join(build_directory, os.path.splitext(os.path.basename(file_path))[0]):s}.o' for file_path in cpp_file_paths]        # noqa: E501
+    executable_path: str = os.path.join(build_directory, f'{executable_name:s}.exe')
+
+    return (cpp_file_paths, object_file_paths, executable_path)
+
+
+if (__name__ == '__main__'):
+
+    executable_name: str = 'present_addition'
+    source_directory: str = os.path.join(os.getcwd(), 'sample_C++_code', 'src')
+
+    (cpp_file_paths,
+     object_file_paths,
+     executable_path) = \
+        get_file_paths(source_directory)
+
+    compile.build_executable_from_source(cpp_file_paths,
+                                         object_file_paths,
+                                         executable_path,
                                          'Debug',
                                          'C++ 2020',
                                          'Disable Compiler Extensions',
