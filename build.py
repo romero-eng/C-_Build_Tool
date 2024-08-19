393119e6d69600caca2e59725ee21a6688af8009
romero-eng <39012010+romero-eng@users.noreply.github.com>
2024-08-19 15:24:18 -0400

    Update build.py

--- build.py
+++ build.py
@@ -6,15 +6,15 @@ import compile
 if (__name__=='__main__'):
 
     executable_name: str = 'present_addition'
+    cpp_file_names: list[str] = ['main', 'Add']
     source_directory: str = os.path.join('sample_C++_code', 'src')
     build_directory: str = os.path.join('sample_C++_code', 'build')
-    cpp_files: list[str] = ['main', 'Add']
 
     if not os.path.exists(build_directory):
         os.mkdir(build_directory)
 
-    compile.build_executable_from_source([os.path.join(source_directory, f'{file:s}.cpp') for file in cpp_files],
-                                         [os.path.join(build_directory, f'{file:s}.o') for file in cpp_files],
+    compile.build_executable_from_source([os.path.join(source_directory, f'{file_name:s}.cpp') for file_name in cpp_file_names],
+                                         [os.path.join(build_directory, f'{file_name:s}.o') for file_name in cpp_file_names],
                                          os.path.join(build_directory, f'{executable_name:s}.exe'),
                                          'Debug',
                                          'C++ 2020',
