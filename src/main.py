8ec223b8ef8e3f2b541e3c82192d684e7136f012
romero-eng <39012010+romero-eng@users.noreply.github.com>
2024-08-24 22:37:40 -0400

    Update main.py

--- src/main.py
+++ src/main.py
@@ -12,7 +12,7 @@ if (__name__ == '__main__'):
 
     try:
 
-        """
+        #"""
         arithmetic_library: compile.CodeBase = \
             compile.CodeBase('Arithmetic',
                              os.path.join(os.getcwd(), 'sample_C++_static_library'))
