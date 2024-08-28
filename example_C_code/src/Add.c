b70a92de0bc0922dd03d6fba5521c995241a0956
romero-eng <39012010+romero-eng@users.noreply.github.com>
2024-08-28 14:19:28 -0400

    Update Add.c
    Continuation of last commit

--- example_C_code/src/Add.c
+++ example_C_code/src/Add.c
@@ -4,18 +4,3 @@ int Arithmetic_add_int_int(int x, int y)
 {
     return x + y;
 }
-
-double Arithmetic_add_double_double(double x, double y)
-{
-    return x + y;
-}
-
-double Arithmetic_add_double_int(double x, int y)
-{
-    return Arithmetic_add_double_double(x, (double) y);
-}
-
-double Arithmetic_add_int_double(int x, double y)
-{
-    return Arithmetic_add_double_double((double) x, y);
-}
