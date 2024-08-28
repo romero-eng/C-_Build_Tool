e2e54e652882960eea6ab1f4a416485ecf778c59
romero-eng <39012010+romero-eng@users.noreply.github.com>
2024-08-28 14:26:40 -0400

    Improved if-else logic around if language standard is recognized or not

--- src/compile.py
+++ src/compile.py
@@ -80,19 +80,25 @@ class CodeBase:
         # Set the language standard, and check to make sure it makes sense
         self._language_standard: str = language_standard
 
+        language_standard_recognized: bool = False
         matched_C_Plus_Plus_standard: re.Match[str] | None = re.fullmatch(r'C\++ 20(\d\d)', self._language_standard)
         matched_C_standard: re.Match[str] | None = re.fullmatch(r'C (19|20)(\d\d)', self._language_standard)
 
         if matched_C_Plus_Plus_standard:
+
             two_digit_year: int = int(matched_C_Plus_Plus_standard.groups()[0])
             if two_digit_year - 11 >= 0:
                 if (two_digit_year - 11) % 3 == 0:
+                    language_standard_recognized = True
                     self._utility: str = 'g++'
                     self._language_standard_flag: str = f'++{flags.C_PLUS_PLUS_LANGUAGE_STANDARDS[int((int(matched_C_Plus_Plus_standard.groups()[0]) - 11)/3)]:s}'  # noqa: E501
                     self._source_code_extensions: list[str] = flags.C_PLUS_PLUS_SOURCE_CODE_EXTENSIONS
+
         elif matched_C_standard:
+
             two_digit_year: int = int(matched_C_standard.groups()[1])
             if two_digit_year in flags.C_LANGUAGE_STANDARDS:
+                language_standard_recognized = True
                 self._utility: str = 'gcc'
                 self._language_standard_flag: str = f'{two_digit_year:2d}'
                 self._source_code_extensions: list[str] = ['.c']
@@ -100,7 +106,8 @@ class CodeBase:
                 # This is not a valid warning for C compilation
                 if 'Follow Effective C++ Style Guidelines' in warnings:
                     warnings.remove('Follow Effective C++ Style Guidelines')
-        else:
+
+        if not language_standard_recognized:
             raise ValueError(f'The following Language Standard is not recognized: {self._language_standard:s}')
 
         # Set the warning, and check to make sure they make sense
--- src/example.py
+++ src/example.py
@@ -14,7 +14,7 @@ if (__name__ == '__main__'):
     clean_up_build_directories: bool = True
     """
 
-    Arithmetic_codebase: CodeBase
+    Arithmetic_codebase: CodeBase | None = None
 
     try:
         """
@@ -36,7 +36,7 @@ if (__name__ == '__main__'):
         Arithmetic_codebase = \
             CodeBase('Arithmetic',
                      Path.cwd()/f'example_C_code',
-                     language_standard='C 2011')
+                     language_standard='C 2018')
         Arithmetic_codebase.generate_as_executable()
 
     except Exception:
@@ -58,5 +58,6 @@ if (__name__ == '__main__'):
                     shutil.rmtree(present_arithmetic_codebase.build_directory)
         """
 
-        if Arithmetic_codebase.build_directory.exists():
-            shutil.rmtree(Arithmetic_codebase.build_directory)
+        if Arithmetic_codebase:
+            if Arithmetic_codebase.build_directory.exists():
+                shutil.rmtree(Arithmetic_codebase.build_directory)
