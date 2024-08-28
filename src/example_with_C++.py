18d3bfbcdd21243ffcfb569918d311269caff986
romero-eng <39012010+romero-eng@users.noreply.github.com>
2024-08-28 15:35:31 -0400

    Renamed variables

--- src/example_with_C++.py
+++ src/example_with_C++.py
@@ -7,38 +7,39 @@ from compile import CodeBase, Dependency
 
 if (__name__ == '__main__'):
 
-    arithmetic_library_codebase: CodeBase | None = None
-    present_arithmetic_codebase: CodeBase | None = None
+    Arithmetic_library_codebase: CodeBase | None = None
+    Arithmetic_codebase: CodeBase | None = None
     use_dynamic_library: bool = False
     clean_up_build_directories: bool = True
 
     try:
-        arithmetic_library_codebase = \
+        Arithmetic_library_codebase = \
             CodeBase('Arithmetic',
                      Path.cwd()/f'example_C++_{'dynamic' if use_dynamic_library else 'static':s}_library',
                      preprocessor_variables=['ADD_EXPORTS'] if use_dynamic_library else [])
 
-        arithmetic_library: Dependency = arithmetic_library_codebase.generate_as_dependency(use_dynamic_library)
+        Arithmetic_library: Dependency = Arithmetic_library_codebase.generate_as_dependency(use_dynamic_library)
 
-        present_arithmetic_codebase = \
+        Arithmetic_codebase = \
             CodeBase('present_arithmetic',
                      Path.cwd()/'example_C++_code')
 
-        present_arithmetic_codebase.add_dependency(arithmetic_library)
-        present_arithmetic_codebase.generate_as_executable()
+        Arithmetic_codebase.add_dependency(Arithmetic_library)
+        Arithmetic_codebase.generate_as_executable()
 
     except Exception:
         print(traceback.format_exc())
 
     else:
-        present_arithmetic_codebase.test_executable()
+        Arithmetic_codebase.test_executable()
 
     finally:
-
         if clean_up_build_directories:
-            if arithmetic_library_codebase:
-                if arithmetic_library_codebase.build_directory.exists():
-                    shutil.rmtree(arithmetic_library_codebase.build_directory)
-            if present_arithmetic_codebase:
-                if present_arithmetic_codebase.build_directory.exists():
-                    shutil.rmtree(present_arithmetic_codebase.build_directory)
+
+            if Arithmetic_library_codebase:
+                if Arithmetic_library_codebase.build_directory.exists():
+                    shutil.rmtree(Arithmetic_library_codebase.build_directory)
+
+            if Arithmetic_codebase:
+                if Arithmetic_codebase.build_directory.exists():
+                    shutil.rmtree(Arithmetic_codebase.build_directory)
--- src/example_with_C++_code_and_C_Library.py
+++ src/example_with_C++_code_and_C_Library.py
@@ -7,41 +7,42 @@ from compile import CodeBase, Dependency
 
 if (__name__ == '__main__'):
 
-    Add_library_codebase: CodeBase | None = None
-    Add_codebase: CodeBase | None = None
+    Arithmetic_library_codebase: CodeBase | None = None
+    Arithmetic_codebase: CodeBase | None = None
     use_dynamic_library: bool = False
     clean_up_build_directories: bool = True
 
     try:
 
-        Add_library_codebase = \
+        Arithmetic_library_codebase = \
             CodeBase('Arithmetic',
                      Path.cwd()/f'example_C_{'dynamic' if use_dynamic_library else 'static':s}_library',
                      language_standard='C 2018',
                      preprocessor_variables=['ADD_EXPORTS'] if use_dynamic_library else [])
 
-        Add_library: Dependency = Add_library_codebase.generate_as_dependency(use_dynamic_library)
+        Arithmetic_library: Dependency = Arithmetic_library_codebase.generate_as_dependency(use_dynamic_library)
 
-        Add_codebase = \
+        Arithmetic_codebase = \
             CodeBase('Arithmetic',
                      Path.cwd()/'example_C++_code_with_C_Linkage',
                      language_standard='C++ 2020')
 
-        Add_codebase.add_dependency(Add_library)
-        Add_codebase.generate_as_executable()
+        Arithmetic_codebase.add_dependency(Arithmetic_library)
+        Arithmetic_codebase.generate_as_executable()
 
     except Exception:
         print(traceback.format_exc())
 
     else:
-        Add_codebase.test_executable()
+        Arithmetic_codebase.test_executable()
 
     finally:
-
         if clean_up_build_directories:
-            if Add_library_codebase:
-                if Add_library_codebase.build_directory.exists():
-                    shutil.rmtree(Add_library_codebase.build_directory)
-            if Add_codebase:
-                if Add_codebase.build_directory.exists():
-                    shutil.rmtree(Add_codebase.build_directory)
+
+            if Arithmetic_library_codebase:
+                if Arithmetic_library_codebase.build_directory.exists():
+                    shutil.rmtree(Arithmetic_library_codebase.build_directory)
+
+            if Arithmetic_codebase:
+                if Arithmetic_codebase.build_directory.exists():
+                    shutil.rmtree(Arithmetic_codebase.build_directory)
