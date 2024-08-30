a8b6fdebbf619a45576ec42b85916a4fd252a1f2
Juan <juan.romero.eng@gmail.com>
2024-08-30 17:13:24 -0400

    Figured out how to compile "fmt"
    Can now successfully compile the 4.x branch from the open-source "fmt" repository

--- src/git.py
+++ src/git.py
@@ -41,6 +41,7 @@ if (__name__=='__main__'):
 
     github_repo_name: str = 'fmt'
     github_username: str = 'fmtlib'
+    github_branch: str = '4.x'
 
     repository_directory: Path = Path.cwd()/'example_repos'/github_repo_name
 
@@ -53,21 +54,26 @@ if (__name__=='__main__'):
                                              None, None)):s}',
                     repository_directory.parent)
 
+        run_command('Test git clone',
+                    f'git checkout {github_branch:s}',
+                    repository_directory)
+        
         for child in repository_directory.iterdir():
             if child.is_file():
                 Path.unlink(child)
-    
+
         for child in repository_directory.iterdir():
             if child.is_dir():
-                if child not in [repository_directory/'src', repository_directory/'include', repository_directory/'.git']:
+                if child not in [repository_directory/github_repo_name, repository_directory/'.git']:
                     shutil.rmtree(child)
 
-        old_include_directory: Path = repository_directory/'include'
-        shutil.move(old_include_directory/github_repo_name, repository_directory/'src')
-        shutil.rmtree(old_include_directory)
+        shutil.move(repository_directory/github_repo_name, repository_directory/'src')
 
     fmt_codebase = \
         CodeBase('Arithmetic',
-                 repository_directory)
+                 repository_directory,
+                 warnings=['Avoid a lot of questionable coding practices',
+                           'Avoid even more questionable coding practices',
+                           'Avoid potentially value-changing implicit conversions'])
     
-    fmt_codebase.generate_as_executable()
+    fmt_codebase.generate_as_dependency(False)
